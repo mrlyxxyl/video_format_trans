ba0313e952540dc4cdd5a36aa66a759bdd27a0de
liyuexin <1290821453@qq.com>
2017-03-21 11:35:00 +0800

    视频格式转换

--- src/com/yx/test/FormatConversion.java
+++ src/com/yx/test/FormatConversion.java
@@ -17,6 +17,7 @@ public class FormatConversion {
         for (int i = 0; i < strings.length; i++) {
             toFlv(strings[i]);
             toMp4(strings[i]);
+            toAvi(strings[i]);
         }
         System.out.println(System.currentTimeMillis() - start);
     }
@@ -34,12 +35,12 @@ public class FormatConversion {
             video.setCodec("flv");
             video.setBitRate(new Integer(180000));
             video.setFrameRate(new Integer(15));
-            EncodingAttributes attrs = new EncodingAttributes();
-            attrs.setFormat("flv");
-            attrs.setAudioAttributes(audio);
-            attrs.setVideoAttributes(video);
+            EncodingAttributes attributes = new EncodingAttributes();
+            attributes.setFormat("flv");
+            attributes.setAudioAttributes(audio);
+            attributes.setVideoAttributes(video);
             Encoder encoder = new Encoder();
-            encoder.encode(source, target, attrs);
+            encoder.encode(source, target, attributes);
         } catch (EncoderException e) {
             e.printStackTrace();
         }
@@ -58,12 +59,34 @@ public class FormatConversion {
             video.setCodec("mpeg4");
             video.setBitRate(new Integer(600000));
             video.setFrameRate(new Integer(15));
-            EncodingAttributes attrs = new EncodingAttributes();
-            attrs.setFormat("mpegvideo");
-            attrs.setAudioAttributes(audio);
-            attrs.setVideoAttributes(video);
+            EncodingAttributes attributes = new EncodingAttributes();
+            attributes.setFormat("mpegvideo");
+            attributes.setAudioAttributes(audio);
+            attributes.setVideoAttributes(video);
             Encoder encoder = new Encoder();
-            encoder.encode(source, target, attrs);
+            encoder.encode(source, target, attributes);
+        } catch (EncoderException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void toAvi(String src) {
+        try {
+            File source = new File(src);
+            File target = new File("e:/" + System.currentTimeMillis() + ".avi");
+            AudioAttributes audio = new AudioAttributes();
+            audio.setCodec("libmp3lame");
+            audio.setBitRate(new Integer(56000));
+            audio.setChannels(new Integer(1));
+            audio.setSamplingRate(new Integer(22050));
+            VideoAttributes video = new VideoAttributes();
+            video.setCodec(VideoAttributes.DIRECT_STREAM_COPY);
+            EncodingAttributes attributes = new EncodingAttributes();
+            attributes.setFormat("avi");
+            attributes.setAudioAttributes(audio);
+            attributes.setVideoAttributes(video);
+            Encoder encoder = new Encoder();
+            encoder.encode(source, target, attributes);
         } catch (EncoderException e) {
             e.printStackTrace();
         }
