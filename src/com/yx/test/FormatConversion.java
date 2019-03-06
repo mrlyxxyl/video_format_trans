1ea4ac3f070997a2a4a66abc88043bad695ee124
qinlingchengxin <34084847+qinlingchengxin@users.noreply.github.com>
2019-03-06 15:41:04 +0800

    新建测试类
    新建测试类

---
+++ src/com/yx/test/FormatConversion.java
@@ -0,0 +1,134 @@
+package com.yx.test;
+
+import it.sauronsoftware.jave.*;
+
+import java.io.File;
+
+/**
+ * 视频格式转换
+ * User: LiWenC
+ * Date: 17-3-20
+ */
+public class FormatConversion {
+
+    public static void main(String[] args) throws EncoderException {
+
+        long start = System.currentTimeMillis();
+        File[] files = new File("E:\\download\\云析学院").listFiles();
+        for (File file : files) {
+            toMp4(file);
+        }
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+    public static void toMp4(File source) {
+        try {
+
+            MultimediaInfo info = new Encoder().getInfo(source);
+            AudioInfo audioTmp = info.getAudio();
+            int audioBitRate = audioTmp.getBitRate();
+            int audioChannels = audioTmp.getChannels();
+            int audioSamplingRate = audioTmp.getSamplingRate();
+
+            VideoInfo videoTmp = info.getVideo();
+            int videoBitRate = videoTmp.getBitRate();
+            float videoFrameRate = videoTmp.getFrameRate();
+
+            String path = source.getAbsolutePath();
+            if (!path.endsWith("wmv")) {
+                return;
+            }
+            path = path.replaceAll("wmv", "mp4");
+            File target = new File(path);
+
+            AudioAttributes audio = new AudioAttributes();
+            audio.setCodec("libfaac");
+            audio.setBitRate(audioBitRate);
+            audio.setChannels(audioChannels);
+            audio.setSamplingRate(audioSamplingRate);
+            VideoAttributes video = new VideoAttributes();
+            video.setCodec("mpeg4");
+            video.setBitRate(videoBitRate);
+            video.setFrameRate((int) videoFrameRate);
+            EncodingAttributes attributes = new EncodingAttributes();
+            attributes.setFormat("mpegvideo");
+            attributes.setAudioAttributes(audio);
+            attributes.setVideoAttributes(video);
+            Encoder encoder = new Encoder();
+            encoder.encode(source, target, attributes);
+        } catch (EncoderException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void toAvi(File source) {
+        try {
+            MultimediaInfo info = new Encoder().getInfo(source);
+            AudioInfo audioTmp = info.getAudio();
+            int audioBitRate = audioTmp.getBitRate();
+            int audioChannels = audioTmp.getChannels();
+            int audioSamplingRate = audioTmp.getSamplingRate();
+            VideoInfo videoTmp = info.getVideo();
+            int videoBitRate = videoTmp.getBitRate();
+            float videoFrameRate = videoTmp.getFrameRate();
+
+            String path = source.getAbsolutePath();
+            path = path.replaceAll("wmv", "avi");
+            File target = new File(path);
+
+            AudioAttributes audio = new AudioAttributes();
+            audio.setCodec("libmp3lame");
+            audio.setBitRate(audioBitRate);
+            audio.setChannels(audioChannels);
+            audio.setSamplingRate(audioSamplingRate);
+            VideoAttributes video = new VideoAttributes();
+
+            video.setCodec(VideoAttributes.DIRECT_STREAM_COPY);
+            video.setBitRate(videoBitRate);
+            video.setFrameRate((int) videoFrameRate);
+            EncodingAttributes attributes = new EncodingAttributes();
+            attributes.setFormat("avi");
+            attributes.setAudioAttributes(audio);
+            attributes.setVideoAttributes(video);
+            Encoder encoder = new Encoder();
+            encoder.encode(source, target, attributes);
+        } catch (EncoderException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void toFlv(File source) {
+        try {
+            MultimediaInfo info = new Encoder().getInfo(source);
+            AudioInfo audioTmp = info.getAudio();
+            int audioBitRate = audioTmp.getBitRate();
+            int audioChannels = audioTmp.getChannels();
+            int audioSamplingRate = audioTmp.getSamplingRate();
+            VideoInfo videoTmp = info.getVideo();
+            int videoBitRate = videoTmp.getBitRate();
+            float videoFrameRate = videoTmp.getFrameRate();
+
+            String path = source.getAbsolutePath();
+            path = path.replaceAll("wmv", "flv");
+            File target = new File(path);
+
+            AudioAttributes audio = new AudioAttributes();
+            audio.setCodec("libmp3lame");
+            audio.setBitRate(audioBitRate);
+            audio.setChannels(audioChannels);
+            audio.setSamplingRate(audioSamplingRate);
+            VideoAttributes video = new VideoAttributes();
+            video.setCodec("flv");
+            video.setBitRate(videoBitRate);
+            video.setFrameRate((int) videoFrameRate);
+            EncodingAttributes attributes = new EncodingAttributes();
+            attributes.setFormat("flv");
+            attributes.setAudioAttributes(audio);
+            attributes.setVideoAttributes(video);
+            Encoder encoder = new Encoder();
+            encoder.encode(source, target, attributes);
+        } catch (EncoderException e) {
+            e.printStackTrace();
+        }
+    }
+}
